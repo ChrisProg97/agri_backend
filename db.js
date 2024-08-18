5ba148d13460c9f49564e788f5a00d77fd8689a1
ChrisProg97 <chrispinjossam@gmail.com>
2024-08-18 18:05:11 +0200

    deleted ca.pem and modified db.js

--- db.js
+++ db.js
@@ -1,21 +1,11 @@
-const fs = require('fs');
+// db.js
 const pgp = require('pg-promise')();
-
 const connectionString = process.env.PG_CONNECTION_STRING;
 
 if (!connectionString) {
     throw new Error('PG_CONNECTION_STRING is not set in the environment variables');
-}
+  }
 
-const sslConfig = {
-    ssl: {
-        rejectUnauthorized: true, // Make sure the certificate is verified
-        ca: fs.readFileSync('./ca.pem').toString() // Read and include the certificate
-    }
-};
-
-const db = pgp({
-    connectionString: connectionString
-});
+const db = pgp(connectionString);
 
 module.exports = db;
