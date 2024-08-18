4cd1bd44353a2530b722852c95bb1447ac6edb46
ChrisProg97 <chrispinjossam@gmail.com>
2024-08-18 18:03:21 +0200

    Added cors

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
-// db.js
+const fs = require('fs');
 const pgp = require('pg-promise')();
+
 const connectionString = process.env.PG_CONNECTION_STRING;
 
 if (!connectionString) {
     throw new Error('PG_CONNECTION_STRING is not set in the environment variables');
-  }
+}
 
-const db = pgp(connectionString);
+const sslConfig = {
+    ssl: {
+        rejectUnauthorized: true, // Make sure the certificate is verified
+        ca: fs.readFileSync('./ca.pem').toString() // Read and include the certificate
+    }
+};
+
+const db = pgp({
+    connectionString: connectionString
+});
 
 module.exports = db;
