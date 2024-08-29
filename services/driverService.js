5425ec7bb50e661bf249ccfc493c2124de9dd135
ChrisProg97 <chrispinjossam@gmail.com>
2024-08-29 08:57:34 +0200

    Added car type field

--- services/driverService.js
+++ services/driverService.js
@@ -1,12 +1,12 @@
 const db = require('../db');
 
-const addDriver = async (firstName, lastName, phone1, phone2, marketId) => {
+const addDriver = async (firstName, lastName, carType, phone1, phone2, marketId) => {
     try {
         const result = await db.query(
-            `INSERT INTO driver (first_name, last_name, phone_1, phone_2, market_id)
+            `INSERT INTO driver (first_name, last_name, car_type, phone_1, phone_2, market_id)
             VALUES ($1, $2, $3, $4, $5)
-            RETURNING id, created_at, first_name, last_name, phone_1, phone_2, market_id`,
-            [firstName, lastName, phone1, phone2, marketId]
+            RETURNING id, created_at, first_name, car_type, last_name, phone_1, phone_2, market_id`,
+            [firstName, lastName, carType, phone1, phone2, marketId]
         );
         return result;
     } catch (error) {
@@ -38,14 +38,14 @@ const getDriverById = async (id) => {
     }
 };
 
-const updateDriver = async (id, firstName, lastName, phone1, phone2, marketId) => {
+const updateDriver = async (id, firstName, lastName, carType, phone1, phone2, marketId) => {
     try {
         const result = await db.query(
             `UPDATE driver
-            SET first_name = $1, last_name = $2, phone_1 = $3, phone_2 = $4, market_id = $5
+            SET first_name = $1, last_name = $2, car_type = $car_type, phone_1 = $3, phone_2 = $4, market_id = $5
             WHERE id = $6
-            RETURNING id, created_at, first_name, last_name, phone_1, phone_2, market_id`,
-            [firstName, lastName, phone1, phone2, marketId, id]
+            RETURNING id, created_at, first_name, car_type, last_name, phone_1, phone_2, market_id`,
+            [firstName, lastName, carType, phone1, phone2, marketId, id]
         );
         if (result.rows.length === 0) {
             throw new Error('Driver not found');
